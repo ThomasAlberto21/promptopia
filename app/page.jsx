99130b781f4e1e06581f85d0d325bd23c2c921a6
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-04 10:05:36 +0700

    .

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
 const Home = () => {
- return <div>Home</div>;
+ return (
+  <section className='flex-col w-full flex-center'>
+   <h1 className='text-center head_text'>
+    Discover & Share
+    <br className='max-md:hidden' />
+    <span className='text-center orange_gradient'>AI-Powred Prompts</span>
+   </h1>
+   <p className='text-center desc'>
+    Promptopia is an open-source AI prompting tool for modern world to discover
+    , create and share creative prompts
+   </p>
+
+   {/* Feed */}
+  </section>
+ );
 };
 
 export default Home;
